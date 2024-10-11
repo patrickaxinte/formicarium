e79b447fbec1aed3fe96236fbf9b91802db9dca5
Patrick Axinte <axinte.re.patrick@student.utcluj.ro>
2024-10-11 18:22:15 +0300

    First commit, added Formicarium Spring Boot project structure

---
+++ src/main/java/com/example/formicarium_app/FormicariumApplication.java
@@ -0,0 +1,13 @@
+package com.example.formicarium_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormicariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormicariumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/formicarium_app/FormicariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formicarium_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormicariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
